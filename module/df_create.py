dfadf661734b4d047a4c13f54598f20d68e66f35
Kim Borchart <kim-pascal.borchart@student.hpi.de>
2019-05-22 11:54:55 +0200

    smoking cleaning
    Co-Authored-By: Justus.Hildebrand@student.hpi.de

--- module/df_create.py
+++ module/df_create.py
@@ -20,7 +20,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 
 client = pymongo.MongoClient('localhost', 27017)  # connects to mongodb server
 db = client.ukbb  # select database on server ('use ukbb' in shell)
-collection = db['ahriWithGeoArray']
+collection = db['ahriML']
 #pprint.pprint(collection.find_one())  # returns one document from temp4 collection ('db.temp4.findOne()' in shell
 #pprint.pprint(collection.find_one({  # mongo documents are noted as python dictionaries, arrays as python arrays.
 #    'yearOfBirth': 1950,
@@ -32,13 +32,56 @@ cursor = collection.find().limit(100000)  # TODO: dont limit in production
 df = pd.DataFrame(list(cursor))
 df.replace("NA", np.nan, inplace=True)
 df = df.dropna(subset=["COPD"])
-df.drop(inplace=True, columns = ["yearOfBirth", "monthOfBirth", "height", "weight", "WheezeInChestInLastYear", "alcoholFrequency", "breathShortness", "dateOfAttendingAssessmentCentre", "diabetes", "_id", "diastolicBloodPressure", "generalPainForThreeMonths", "geolocationEast", "geolocationNorth", "noOfCigarettesPerDay", "noOfCigarettesPreviouslyPerDay", "patientId", "smoking", "smokingStatus", "stoppedSmokingFor6Months", "systolicBloodPressure"])
+#df.drop(inplace=True, columns = ["_id", "patientId"])
+
+df["neverSmoked"] = np.nan
+df["previouslySmoked"] = np.nan
+df["currentlySmoking"] = np.nan
+
+
+
+for index, row in df.iterrows():
+    row["systolicBloodPressure0"] = (row["systolicBloodPressure0"] + row["systolicBloodPressure1"]) / 2
+    row["diastolicBloodPressure0"] = (row["diastolicBloodPressure0"] + row["diastolicBloodPressure1"]) / 2
+
+    if row["noOfCigarettesPreviouslyPerDay"] == np.nan:
+        row["noOfCigarettesPreviouslyPerDay"] = 0
+
+    if row["noOfCigarettesPerDay"] == np.nan:
+        row["noOfCigarettesPerDay"] = 0
+    elif row["noOfCigarettesPerDay"] < 0:
+        row["noOfCigarettesPerDay"] = np.nan
+
+    if row["smokingStatus"] == 0:
+        row["neverSmoked"] = 1
+        row["previouslySmoked"] = 0
+        row["currentlySmoking"] = 0
+    elif row["smokingStatus"] == 1:
+        row["neverSmoked"] = 0
+        row["previouslySmoked"] = 1
+        row["currentlySmoking"] = 0
+    elif row["smokingStatus"] == 2:
+        row["neverSmoked"] = 0
+        row["previouslySmoked"] = 0
+        row["currentlySmoking"] = 1
+
+    if row["smoking"] == 2:
+        row["noOfCigarettesPerDay"] = 1
+        row["neverSmoked"] = 0
+        row["previouslySmoked"] = 0
+        row["currentlySmoking"] = 1
+
+
+df.drop(inplace=True, columns=["systolicBloodPressure1", "diastolicBloodPressure1"])
+df.rename(index=str, inplace=True, columns={"systolicBloodPressure0": "systolicBloodPressure", "diastolicBloodPressure0": "diastolicBloodPressure"})
+df = df.dropna(subset=["diastolicBloodPressure", "weight", "height", "numberOfCigarettesPerDay", "smoking", "smokingStatus"])
+
 
 y = df["sputumOnMostDays"]
 X = df.drop(columns=["sputumOnMostDays"])
-#pfr = pandas_profiling.ProfileReport(df)
-#pfr.to_file("/tmp/df_report.html")
-#df.describe()
+pfr = pandas_profiling.ProfileReport(df)
+pfr.to_file("/tmp/df_report22_5.html")
+df.describe()
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, random_state=1337)
 
 #reg = linear_model.ElasticNet() #score: -1.395
@@ -47,7 +90,7 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, random_
 #reg = linear_model.Lasso() #score: -1.395
 #reg = svm.SVR() #0.098
 #reg = GradientBoostingRegressor() #0.27
-
+reg = svm.SVR(kernel='rbf')
 
 
 reg.fit(X_train, y_train)
@@ -62,7 +105,7 @@ difference.sort_values(inplace=True)
 plt.figure()
 plt.plot(range(0, 6000), difference[:6000])
 #plt.plot(range(0, 100), predictions[100], label="predictions")
-plt.savefig('/tmp/ensemble.png')
+plt.savefig('/tmp/rbf.png')
 
 def flatten(np_series):
     flat_list = np.Series()
