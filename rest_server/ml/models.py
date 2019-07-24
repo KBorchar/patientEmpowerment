1d0cc5351dc3d9583eafc8af5c9bcf3126c1a7d5
Justus Hildebrand <justus.hildebrand@student.hpi.de>
2019-07-24 16:42:59 +0200

    move model JSON generation to IO

--- rest_server/ml/models.py
+++ rest_server/ml/models.py
@@ -1,21 +1,2 @@
-from ml import model_objects, imputer, dataframe_column_labels
 
-# converts
-def get_model_dict(label):
-    model = model_objects[label]
-    feature_names = dataframe_column_labels.copy()
-    label_index = feature_names.index(label)
-    del(feature_names[label_index])
-    feature_means = imputer.initial_imputer_.statistics_.copy().tolist()
-    del(feature_means[label_index])
-    weights = model.coef_
-    model_dict = dict()
-    model_dict["intercept"] = model.intercept_[0]
-    features_dict = dict()
-    for i, feature_name in enumerate(feature_names):
-        feature_dict = dict()
-        feature_dict["coef"] = weights[0][i]
-        feature_dict["mean"] = feature_means[i]
-        features_dict[feature_name] = feature_dict
-    model_dict["features"] = features_dict
-    return model_dict
+
