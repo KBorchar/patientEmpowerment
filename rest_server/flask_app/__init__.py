44f37d917d551e368f9b74ffd4dfbb35d8089d14
Justus Hildebrand <justus.hildebrand@student.hpi.de>
2019-07-25 12:30:42 +0200

    more comments for methods

--- rest_server/flask_app/__init__.py
+++ rest_server/flask_app/__init__.py
@@ -1,4 +1,6 @@
 # initializes flask_app package
+# every file in this package has access to the 'app' variable which is the global server instance
+# from the Flask framework
 from flask import Flask
 
 app = Flask(__name__)
--- rest_server/flask_app/request_parser.py
+++ rest_server/flask_app/request_parser.py
@@ -1,7 +1,9 @@
 from flask import abort
 from pandas import DataFrame
 
-def check_for_Nones(json, dicts_to_check=[], arrays_to_check=[]):
+# takes a parsed JSON, checks the given dicts and lists for None values that came in from the outside.
+# Currently we will not support Nones in the requests so we return errors.
+def check_for_Nones(json, dicts_to_check=[], lists_to_check=[]):
     if json is None:
         abort(415, description='Please provide a JSON file in the request body.')
     if None in json.values():
@@ -9,33 +11,37 @@ def check_for_Nones(json, dicts_to_check=[], arrays_to_check=[]):
     for field in dicts_to_check:
         if None in json[field].values():
             abort(417, description='No null values in JSON fields allowed.')
-    for field in arrays_to_check:
+    for field in lists_to_check:
         if None in json[field]:
             abort(417, description='No null values in JSON fields allowed.')
 
 def get_db_and_collection_name(json):
     return json['db'], json['collection']
 
+# receives labels for prediction and user_data for the instance to predict from the request JSON
 def parse_predict_request(request):
     json = request.get_json()
-    check_for_Nones(json, dicts_to_check=['user_data'], arrays_to_check=['labels'])
+    check_for_Nones(json, dicts_to_check=['user_data'], lists_to_check=['labels'])
     labels = json['labels']
     user_data = DataFrame.from_records([json['user_data']])
     return labels, user_data
 
+# receives the list of labels for which the model data were requested
 def parse_get_models_request(request):
     json = request.get_json()
-    check_for_Nones(json, arrays_to_check=['labels'])
+    check_for_Nones(json, lists_to_check=['labels'])
     labels = json['labels']
     return labels
 
+# receives the database name, collection name and list of labels for which the models are supposed to be retrained
 def parse_relearn_models_request(request):
     json = request.get_json()
-    check_for_Nones(json, arrays_to_check=['labels'])
+    check_for_Nones(json, lists_to_check=['labels'])
     db, collection = get_db_and_collection_name(json)
     labels = json['labels']
     return db, collection, labels
 
+# receives the database and collection name for which the feature_config was requested
 def parse_get_feature_config(request):
     json = request.get_json()
     check_for_Nones(json)
